a57d8d8f7739a6811f30bef2fd4a38c17690cf44
Matheusmarazzi <71531067+Matheusmarazzi@users.noreply.github.com>
2020-10-13 21:26:33 -0300

    Add files via upload

---
+++ exercicio 1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B;
+            Console.Write("Digite o valor de A: ");
+            A = int.Parse(Console.ReadLine());
+            Console.Write("Digite o valor de B: ");
+            B = int.Parse(Console.ReadLine());
+            if (A > B)
+                Console.WriteLine(" o maior valor é: " + A);
+            else
+                Console.WriteLine(" o maior valor é: " + B);
+            Console.ReadKey();
+
+        
+        }
+    }
+}
